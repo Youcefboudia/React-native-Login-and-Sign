71d4876181dfad47d8489b24464b64125b3fb74b
Youcefboudia <youcef.boudia.347@gmail.com>
2024-06-02 18:37:20 +0100

    first commit

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,19 +1,80 @@
 import React, { useState } from 'react';
 import {Text, View, Image, TouchableOpacity, Alert, Platform } from 'react-native';
 import styles from '../assets/styles/stylesHome';
-import { TextInput, ScrollView } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import { TextInput, FlatList } from 'react-native';
 
 
+const data = [
+    { id: '1', location: 'Hg3+QFJ, Laghouat' },
+    { id: '2', location: 'Charchafa, Laghouat' },
+    { id: '3', location: 'Al ghouati, Aflou' },
+    { id: '4', location: 'RV93+QFJ, Kheng' },
+    { id: '5', location: 'RV93+QFJ, Laghouat' },
+    { id: '6', location: 'RV93+QFJ, Laghouat' },
+    { id: '7', location: 'RV93+QFJ, Laghouat' },
+    { id: '8', location: 'RV93+QFJ, Laghouat' },
+    { id: '9', location: 'RV93+QFJ, Laghouat' },
+    { id: '10', location: 'RV93+QFJ, Laghouat' },
+  ];
 export default Home=({navigation})=>{
 
+
+  const [search, setSearch] = useState('');
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleSearch = (text) => {
+    setSearch(text);
+    const filtered = data.filter(item => item.location.toLowerCase().includes(text.toLowerCase()));
+    setFilteredData(filtered);
+  };
     return(
-        <ScrollView style={styles.scrollContainer}>
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text>Home</Text>
+        <View style={styles.scrollContainer}>
+            <View style={styles.topHead}>
+            <View style={styles.appHead}>
+                <View style={styles.appName}>
+                    <Image style={styles.imgHead} source={require('../assets/LOGO.png')} />
                 </View>
+                <TouchableOpacity>
+            <Image style={styles.imgHead} source={require('../assets/lens.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity 
+                onPress={() => navigation.navigate('Sign')}>
+                <View style={styles.appName}>
+                    <Image style={styles.imgHead} source={require('../assets/account.png')} />
+                </View>
+                </TouchableOpacity>
             </View>
-        </ScrollView>
+            <View style={styles.header}>
+                    <TextInput style={styles.inputSearch}
+                        value = {search}
+                        placeholder="Search for pharmaciy"
+                        placeholderTextColor="#929292"
+                        onChangeText={handleSearch}
+                        />
+                    <TouchableOpacity>
+                    <Image style={styles.searchImg} source={require('../assets/search.png')} />
+                    </TouchableOpacity>
+                </View>
+                </View>
+            <View style={styles.container}>
+                
+                <FlatList
+                    data={filteredData}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                    <View style={styles.cntContent}>
+                        <View style={styles.textElemnt}>
+                        <Image style={styles.searchImg} source={require('../assets/location.png')} />
+                        <Text style={styles.textLocation}>{item.location}</Text>
+                        <TouchableOpacity style={styles.detailsBtn}>
+                            <Image style={styles.searchImg} source={require('../assets/info.png')} />
+                        </TouchableOpacity>
+                        </View>
+                    </View>
+                    )}
+                    contentContainerStyle={styles.listContentContainer}
+                />
+            </View>
+        </View>
     )
 }
--- assets/styles/stylesHome.js
+++ assets/styles/stylesHome.js
@@ -1,21 +1,122 @@
 import { StyleSheet } from "react-native";
-import COLORS from "../../constants/Colors";
 
 const styles = StyleSheet.create({
     scrollContainer:{
         flex: 1, backgroundColor: '#e8ecf4'
     },
+    topHead:{
+      backgroundColor:'#1d3557',
+      paddingHorizontal:10
+    },
+    appHead:{
+      marginTop:20,
+    flexDirection:"row",
+    alignContent:'center',
+    justifyContent:"space-between",
+    alignItems:"center",
+    marginBottom:10,
+    padding:10,
+    },
+    appName:{
+      flexDirection:'row',
+      alignItems:'center',
+      backgroundColor:'#f2f2f2',
+      borderRadius:50,
+      padding:5
+    },
+    textHead:{
+      marginHorizontal:15,
+      fontSize:12,
+      fontWeight:"600",
+      padding:5,
+      color:"gray"
+    },
+
+    imgHead: {
+      width:30,
+      height:30,
+    },
 
   container: {
+    flex:1,
     padding: 12,
-    flex: 1,
   },
 
   header: {
-    margin: 24,
-    flex: 1,
+    width:'100%',
+    alignItems:'center',
+    justifyContent:"center",
+    flexDirection:'row',
+
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    padding:5,
+    elevation: 50,
+    borderRadius:15,
+    marginBottom:20,
+    backgroundColor:"#f2f2f2",
+    elevation: 50,
+    
+  },
+  
+  searchBtn: {
+    width:40,
+    height:40,
+    padding:10,
+    width:'10%',
+    alignItems:'center',
+    justifyContent:"center"
+  },
+  searchImg: {
+    width:30,
+    height:30,
+    padding:10,
+    marginHorizontal:10
   },
 
+  inputSearch: {
+    margin:5,
+    paddingHorizontal:5,
+    width:'80%',
+    borderWidth: 0,
+    borderColor: 'gray',
+    fontSize:12
+  },
+
+
+  titleContent:{
+    fontSize:24,
+    fontWeight:"400",
+    alignSelf:'center',
+    marginTop:10,
+    marginBottom:20,
+    color:"#000",
+  },
+  cntContent:{
+    flex:1,
+    backgroundColor:"#fff",
+    flexDirection:"column",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    padding:10,
+    elevation: 50,
+    borderRadius:7,
+    marginBottom:20,
+
+  },
+  textElemnt:{
+    flex:1,
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center"
+  },
+  textLocation:{
+      fontSize:12,
+      color:"#929292",
+      fontWeight:"500",
+  }
 })
 
 export default styles;
